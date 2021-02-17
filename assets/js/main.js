db12712b8cbb843fe7cc80875996b6cf5dac8834
Santi Espina <santi.espina14@gmail.com>
2021-02-17 14:49:07 -0300

    falta dark/light mode

---
+++ assets/js/main.js
@@ -0,0 +1,81 @@
+/*==================== SHOW MENU ====================*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+    
+    // Validate that variables exist
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*==================== SHOW SCROLL TOP ====================*/ 
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
+/*==================== DARK LIGHT THEME ====================*/ 
+
+
+/*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/ 
+
+
+/*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/ 
+
+
+/*==================== GENERATE PDF ====================*/ 
+// PDF generated area
+
+
+// Html2pdf options
+
+
+// Function to call areaCv and Html2Pdf options 
+
+
+// When the button is clicked, it executes the three functions
+
+    // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+
+
+    // 2. The PDF is generated
+
+
+    // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
